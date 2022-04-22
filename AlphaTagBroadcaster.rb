52f8ee9a2663aa93e61e9cd4c561bc33320f2913
Jason Becht <jason@cryptechstudios.com>
2022-04-22 12:01:43 -0500

    looping and printing

--- AlphaTagBroadcaster.rb
+++ AlphaTagBroadcaster.rb
@@ -80,7 +80,7 @@ def postAlphaTag(alphaTag)
   request = Net::HTTP::Get.new(uri.path)#, 'Content Type' => 'application/json')
   # request.basic_auth @icecastUser, @icecastPass
   ap "Basic " + Base64::encode64("#{@icecastUser}:#{@icecastPass}")
-  request['Authorization'] =  "Basic " + Base64::encode64("#{@icecastUser}:#{@icecastPass}")
+  request['Authorization'] =  "Basic " + Base64::strict_encode64("#{@icecastUser}:#{@icecastPass}")
   resp = http.request(request)
   ap resp.body
 
