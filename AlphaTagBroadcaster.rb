43dc9262a5df7df56e70181e10416e778bbbcb1b
Jason Becht <jason@cryptechstudios.com>
2022-04-22 07:51:43 -0500

    intial commit

--- AlphaTagBroadcaster.rb
+++ AlphaTagBroadcaster.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
 #
-#
-#
+require 'rubyserial'
+
+serialport = Serial.new '/dev/ttyACM0' # Defaults to 9600 baud, 8 data bits, and no parity
+serialport = Serial.new '/dev/ttyACM0', 57600
+serialport = Serial.new '/dev/ttyACM0', 19200, 8, :even
